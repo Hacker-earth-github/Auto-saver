c05fc9dc5aedd46a19c0c4ca240b6f2527ec53e6
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2024-04-16 19:40:51 +0100

    update-commit

--- packages/react-app/src/components/Home.jsx
+++ packages/react-app/src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+
 const Home = () => {
   return (
-    <section>
-      Home
+    <section className="home">
+      <div className="all">
+      <div className="homeCon">
+          <h1>Let GetStart</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, nisi!</p>
+       </div>
+          <div className="btnCon">
+            <button className="btnBlue">Lookup</button>
+            <button className="btnWhite">Set it Up</button>
+          </div>
+      </div>
+       
     </section>
   );
 };
 
 export default Home;
+
